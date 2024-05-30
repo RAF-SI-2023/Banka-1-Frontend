5f9722cc7181475042e24eb500e68646d20e63f1
vnikolic7821rn <vnikolic7821rn@raf.rs>
2024-05-30 11:09:52 +0200

    Added production configuration and fileReplacements statement

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const prodenvironment = {
+    production: true,
+    userService: 'http://banka-1-dev.si.raf.edu.rs/user-service/api',
+    marketService: 'http://banka-1-dev.si.raf.edu.rs/market-service/api',
+    fetchingService: 'http://banka-1-dev.si.raf.edu.rs/fetching-service/api'
+}
