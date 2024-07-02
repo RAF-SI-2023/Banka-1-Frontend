fa83a81e74be2e81f65058bc31cc1ee58b26db14
vnikolic7821rn <vnikolic7821rn@raf.rs>
2024-07-02 02:30:38 +0200

    e2e tests

--- cypress/e2e/employee-list/admin-employees.cy.js
+++ cypress/e2e/employee-list/admin-employees.cy.js
@@ -1,12 +1,12 @@
 describe('check number of employees for admin user', () => {
     it('logged in as admin and 4 employees are displayed', () => {
-        cy.visit('http://localhost/login');
+        cy.visit('http://localhost:4200/login');
         cy.contains('Employee').click();
         cy.get('label').contains('Email').click();
         cy.get('label').contains('Email').siblings('input').type('admin');
         cy.get('input[type="password"]').click().type('admin');
         cy.get('button').contains('Login').click();
         cy.contains('div', 'Employee panel').first().click();
-        cy.get('.tabs-content app-dynamic-table tbody tr').should('have.length', 0);
+        cy.get('.tabs-content app-dynamic-table tbody tr').should('have.length', 4);
     })
 });
